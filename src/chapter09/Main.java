98c815f6e846bb8eb083284f931fd60fb630db91
kimhyunso <gustn9515@gmail.com>
2024-11-15 17:12:40 +0900

    feat: 실습예제 구현

--- src/chapter09/Main.java
+++ src/chapter09/Main.java
@@ -11,12 +11,20 @@ public class Main {
         System.out.println("max: " + (max + 1L));
 
         Scanner input = new Scanner(System.in);
-        System.out.print("두 정수를 입력해주세요: ");
-        int x = input.nextInt();
-        int y = input.nextInt();
+//        System.out.print("두 정수를 입력해주세요: ");
+//        int x = input.nextInt();
+//        int y = input.nextInt();
+//
+//        double result = (x + y) / 2.0;
+//
+//        System.out.printf("avg: %.2f", result);
 
-        double result = (x + y) / 2.0;
+        System.out.print("초를 입력해주세요: ");
+        int second = input.nextInt();
+        int h = second / 60 / 60;
+        int m = second / 60 % 60;
+        int s = second % 60;
 
-        System.out.printf("avg: %.2f", result);
+        System.out.printf("%d초는 %d시간 %02d분 %02d초 입니다.", second, h, m, s);
     }
 }
