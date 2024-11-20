e74ad9043acf7983584c8c22e5d8970e3a0efeed
kimhyunso <gustn9515@gmail.com>
2024-11-20 19:11:08 +0900

    feat: 실습 10-1

---
+++ src/chapter10/Main4.java
@@ -0,0 +1,23 @@
+package chapter10;
+
+import java.util.Scanner;
+
+public class Main4 {
+    public static void main(String[] args) {
+
+        int data = 0x11223344;
+        System.out.printf("%d\n", data);
+        System.out.printf("%08X\n", data & 0xFFFF0000);
+        System.out.printf("%08X\n", data | 0xFFFF0000);
+        System.out.printf("%08X\n", data ^ 0xFFFF0000);
+        System.out.printf("%08X\n", data ^ 0xFFFF0000);
+        System.out.printf("%08X\n", ~data);
+
+
+        Scanner input = new Scanner(System.in);
+
+        int x = input.nextInt(), y = input.nextInt();
+        int result = x + (~y + 1);
+        System.out.println(result);
+    }
+}
