7d9e4c238f722411b5070368ae010b34d20a7563
kimhyunso <gustn9515@gmail.com>
2024-12-26 15:26:50 +0900

    feat: recursivecall

--- src/chapter14/Main7.java
+++ src/chapter14/Main7.java
@@ -7,6 +7,15 @@ public class Main7 {
         Scanner input = new Scanner(System.in);
         String str = input.next();
         chatAt(str.length() - 1, str);
+        System.out.println();
+        chatAtVersion2(str.length() - 1, str);
+    }
+
+    public static void chatAtVersion2(int index, String str) {
+        if (index < 0)
+            return;
+        System.out.print(str.charAt(index));
+        chatAt(index - 1, str);
     }
 
     public static char chatAt(int index, String str) {
